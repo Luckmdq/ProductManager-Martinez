c0beb7687b8122ac7a67d34f6e6fe2ec61bff252
LucianoMartinez <Lucianom764@gmail.com>
2023-12-16 16:33:27 -0300

    implementacion de axios para obtencion de productos

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,17 +1,19 @@
+import Axios from "axios";
 import { Router } from "express";
 
-const views=Router();
-let data={};
+const views = Router();
 
-views.get('/', (req,res)=>{
-    data={title:"home"}
-    return res.render("home",data);
-})
+views.get("/", async (req, res) => {
+  const { data: productos } = await Axios.get(
+    "http://localhost:8080/api/products"
+  );
+  console.log(productos);
+  return res.render("home", productos);
+});
 
-views.get('/realtimeproducts', (req,res)=>{
-    data={title:"productos"}
-    return res.render("realTimeProducts",data);
-})
+views.get("/realtimeproducts", (req, res) => {
+  data = { title: "productos" };
+  return res.render("realTimeProducts", data);
+});
 
-
-export default views
+export default views;
